a99a48832b1d60eb845197d467ee9ac382927b3f
Mihir Rathod <mihirr.rathodd@gmail.com>
2025-06-11 14:01:33 -0400

    Add 2 more GET endpoints to fetch by ID and fetch a list of notes with matching titles

--- src/main/java/com/mihir/notesapi/NoteController.java
+++ src/main/java/com/mihir/notesapi/NoteController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 @RestController
 @RequestMapping("/notes")
@@ -13,14 +14,26 @@ public class NoteController {
     // a simple in-memory list to store notes (will replace this later with a database)
     private List<Note> notes = new ArrayList<>();
 
-    // created a GET API endpoint to the list of all notes
+    // GET API endpoint to display the list of all notes
 
     @GetMapping // responds to HTTP GET at /notes and returns all notes
     public List<Note> getAllNotes() {
         return notes;
     }
 
-    // created a POST API endpoint to add new notes in the list of all nodes
+    // GET API endpoint to fetch the note by ID
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Note> getNoteById(@PathVariable int id) {
+        for (Note note : notes){
+            if (note.getId() == id){
+                return ResponseEntity.ok(note);
+            }
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    // POST API endpoint to add new notes in the list of all nodes
 
     @PostMapping // responds to HTTP POST at /notes, reads a Note from the request body (JSON), adds it to the list, and returns it
     public Note addNote(@RequestBody Note note) {
@@ -28,7 +41,7 @@ public class NoteController {
         return note;
     }
 
-    // create a PUT API endpoint to update the existing notes in the list of all nodes
+    // PUT API endpoint to update an existing note by ID in the list of all nodes
 
     @PutMapping("/{id}")
     public ResponseEntity<Note> updateNote(@PathVariable int id, @RequestBody Note updatedNote) {
@@ -42,7 +55,7 @@ public class NoteController {
         return ResponseEntity.notFound().build(); // HTTP 404
     }
 
-    // create a DELETE API endpoint (delete note)
+    // DELETE API endpoint to delete a note
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Note> deleteNote(@PathVariable int id){
@@ -55,5 +68,21 @@ public class NoteController {
         }else{
             return ResponseEntity.notFound().build(); // HTTP 404
         }
+    }
+
+    // GET API endpoint which can fetch a list of notes with matching titles of the notes.
+
+    @GetMapping("/search") //
+    public ResponseEntity<List<Note>> searchNotesByTitle(@RequestParam String title) {
+        List<Note> searchResults = new ArrayList<>();
+        for (Note note : notes){
+            if (note.getTitle().toLowerCase().contains(title.toLowerCase())){
+                searchResults.add(note);
+            }
+        }
+        if (searchResults.isEmpty()){
+            return ResponseEntity.noContent().build(); // HTTP 204 if nothing found
+        }
+        return ResponseEntity.ok(searchResults);
     }
 }
